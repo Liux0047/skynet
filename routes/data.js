1765632a4444b7ed7747ddfe8f098a176bc8dfe9
Allen <tracy1kobe@gmail.com>
2017-03-05 17:28:24 +0800

    watch list and added new data points

--- routes/data.js
+++ routes/data.js
@@ -2,11 +2,21 @@ var express = require('express');
 var router = express.Router();
 
 var data = {};
+var watchlist = [];
 parseCSV(data);
 
 /* GET portfoloi data */
 router.get('/portfolio/:id', function(req, res, next) {
   res.json(data[req.params.id]);
+});
+
+router.get('/watchlist/:id', function(req, res, next) {
+  watchlist.push(req.params.id);
+  res.send("added");
+});
+
+router.get('/watchlist', function(req, res, next){
+  res.json(watchlist);
 });
 
 
@@ -19,7 +29,7 @@ module.exports = router;
 
 
 function parseCSV(data) {
-  var columns = ['portfolioId', 'Name', 'NAV', 'Change', 'YTD'];
+  var columns = ['portfolioId', 'Name', 'Ticker', 'NAV', 'Change', 'YTD', 'currency'];
   ['hk-one', 'sg-one', 'au-one', 'global-products'].forEach(f => {
     require('csv-to-array')({
       file: 'data/' + f + '.csv',
