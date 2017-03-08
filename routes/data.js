ef227fdd5689170b0132e0a25b2985635ba95357
Allen <tracy1kobe@gmail.com>
2017-03-08 13:41:29 +0800

    bug fix

--- routes/data.js
+++ routes/data.js
@@ -13,15 +13,13 @@ router.get('/portfolio/:id', function(req, res, next) {
 });
 
 router.post('/portfolio', function(req, res, next){
-  if (req.body.action === 'performance'){
-    var perfData = data[req.body.fund];
-    var answer = 'The NAV for ' + perfData.name + ' is ' + perfData.Ccy + perfData.NAV + ', with ' + perfData.ChangePercent + '% from yesterday.'
-    res.json({
-      'speech': answer,
-      'source': 'Allen API',
-      'displayText': answer
-    });
-  }
+  var perfData = data[req.body.fund];
+  var answer = 'The NAV for ' + perfData.Name + ' is ' + perfData.Ccy + ' ' + perfData.NAV + ', with ' + perfData.ChangePercent + '% change from yesterday.';
+  res.json({
+    'speech': answer,
+    'source': 'Allen API',
+    'displayText': answer
+  });
 })
 
 router.post('/test', function(req, res, next){
